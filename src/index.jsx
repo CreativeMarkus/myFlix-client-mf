ef0f00ed7a75285682e09ad44059e71e6a4a1246
CreativeMarkus <mark@creativemarkus.com>
2025-08-13 20:08:48 -0300

    moved index file

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import MainView from './Components/main-view/main-view';
-import { LoginView } from './Components/login-view/login-view';
+import LoginView from './Components/login-view/login-view';
 import SignupView from './Components/signup-view/signup-view';
 import { ProfileView } from './Components/profile-view/profile-view';
 import MovieView from './Components/movie-view/movie-view';
